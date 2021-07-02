3fb541c75beb3db5cddf76605537e5143b9cc126
Debaditya Shome <56554465+DebadityaShome@users.noreply.github.com>
2021-07-02 18:39:36 +0530

    Try 1

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,6 +1,5 @@
 import { React, useState, useEffect } from "react";
 import sanityClient from "../client";
-import { Link } from "react-router-dom";
 
 export default function Post() {
     const [postData, setPost] = useState(null);
